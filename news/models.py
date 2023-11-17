3c91c010961c316a3c6e56771b43eab6cd7ff45c
lumar0000 <luca.grassi@alessandrinimainardi.edu.it>
2023-11-17 09:55:17 +0100

    passo 16

---
+++ news/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Giornalista(models.Model):
+    """modello generico giornalista"""
+    nome = models.CharField(max_length=20)
+    cognome = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome +" "+ self.cognome
+
+class Articolo(models.Model):
+    """modello generico giornalista"""
+    titolo = models.CharField(max_length=100)
+    contenuto = models.TextField()
+    giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
+
+    def __str__(self):
+        return self.titolo
