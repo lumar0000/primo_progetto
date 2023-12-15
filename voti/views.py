113a950c7b31d13326063dcfc1fcbb1dde948574
lumar0000 <luca.grassi@alessandrinimainardi.edu.it>
2023-12-15 10:57:41 +0100

    voti

--- voti/views.py
+++ voti/views.py
@@ -17,7 +17,7 @@ def view_b(request):
     return render(request, "view_b.html", context)
 
 def view_c(request):
-    voti :  {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
+    voti =  {'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7,4),("Storia",7,4),("Geografia",5,7)],
         'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9,0),("Storia",8,2),("Geografia",8,1)],
         'Nicola Spina':[("Matematica",7,2),("Italiano",6,2),("Inglese",4,3),("Storia",8,2),("Geografia",8,2)]}
     tot = 0
