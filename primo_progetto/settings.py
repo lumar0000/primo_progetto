26d17e69fb229e7e45a0b03abf7d2abc0188b713
lumar0000 <luca.grassi@alessandrinimainardi.edu.it>
2024-02-23 09:09:38 +0100

    passo 24

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'seconda_app',
     'news',
     'voti',
-    'prova_pratica_1'
+    'prova_pratica_1',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'primo_progetto.urls'
@@ -133,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
